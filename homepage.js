42814ba805f7ec178930a06f2f9497cb151fce73
mariiawasif <mariawasif56@gmail.com>
2023-07-21 12:21:49 +0500

    ninth commit

--- homepage.js
+++ homepage.js
@@ -7,29 +7,6 @@ for(let i=0; i<btn.length; i++){
    })   
 }
 
-
-// var navbar = document.querySelector('.navbar');
-// var loggedUsers = JSON.parse(localStorage.getItem('loggedUsers')) || [];
-// if (loggedUsers.length > 0) {
-//   var username = loggedUsers[0].name;
-//   var newLink = document.createElement('a');
-//   newLink.setAttribute('href', '#');
-//   newLink.textContent = "logged in as "+username;
-//   navbar.appendChild(newLink);
-// } else {
-//   var newLink = document.createElement('a');
-//   newLink.setAttribute('href', 'login.html');
-//   newLink.textContent = 'Log In';
-//   navbar.appendChild(newLink);
-// }
-
-// var logoutBtn = document.querySelector('.fa-sign-out-alt');
-// logoutBtn.addEventListener('click', function () {
-//   localStorage.removeItem('loggedUsers');
-//   newLink.textContent = 'Log In';
-//   window.location.href='login.html';
-// });
-
   
 document.addEventListener('DOMContentLoaded', function () {
   var logoutBtn = document.querySelector('.fa-sign-out-alt');
